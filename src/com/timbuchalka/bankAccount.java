939404d5cb94cb304f40db4d0c4a83d1815457ad
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-08 18:33:35 +0200

    Testing out Code --> Generate --> Generate setters and getters

--- src/com/timbuchalka/bankAccount.java
+++ src/com/timbuchalka/bankAccount.java
@@ -8,42 +8,83 @@ public class bankAccount {
     private String emailAddress;
     private int phoneNumber;
 
-    public long getAccountNumber(long accountNumber) {
-        return this.accountNumber;
+//    public long getAccountNumber(long accountNumber) {
+//        return this.accountNumber;
+//    }
+//    public void setAccountNumber(long accountNumber) {
+//        this.accountNumber = accountNumber;
+//    }
+//
+//    public long getBalance(long balance) {
+//        return this.balance;
+//    }
+//    public void setBalance(long balance) {
+//        this.balance = balance;
+//    }
+//
+//    public String getCustomerName(String customerName) {
+//        return this.customerName;
+//    }
+//    public void setCustomerName(String customerName) {
+//        String validName = customerName.toLowerCase();
+//        if (validName.equals("radoslaw")) {
+//            this.customerName = customerName;
+//        } else {
+//            customerName = "Invalid name";
+//        }
+//    }
+//
+//    public String getEmailAddress(String emailAddress) {
+//        return this.emailAddress;
+//    }
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
+//
+//    public int getPhoneNumber(int phoneNumber) {
+//        return this.phoneNumber;
+//    }
+//    public void setPhoneNumber(int phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+
+
+    public long getAccountNumber() {
+        return accountNumber;
     }
+
     public void setAccountNumber(long accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    public long getBalance(long balance) {
-        return this.balance;
+    public long getBalance() {
+        return balance;
     }
+
     public void setBalance(long balance) {
         this.balance = balance;
     }
 
-    public String getCustomerName(String customerName) {
-        return this.customerName;
-    }
-    public void setCustomerName(String customerName) {
-        String validName = customerName.toLowerCase();
-        if (validName.equals("radoslaw")) {
-            this.customerName = customerName;
-        } else {
-            customerName = "Invalid name";
-        }
+    public String getCustomerName() {
+        return customerName;
     }
 
-    public String getEmailAddress(String emailAddress) {
-        return this.emailAddress;
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
-    public int getPhoneNumber(int phoneNumber) {
-        return this.phoneNumber;
+    public int getPhoneNumber() {
+        return phoneNumber;
     }
+
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
